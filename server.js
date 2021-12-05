44d18ee8889fa8ce71be2a67e0da19b47afced54
Djamel Ramdani <enzonpapa@gmail.com>
2021-12-04 19:07:21 -0600

    setup app.use and app.get

--- server.js
+++ server.js
@@ -5,6 +5,26 @@ const PORT = process.env.PORT || 3001;
 const fs = require("fs");
 const path = require("path");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
