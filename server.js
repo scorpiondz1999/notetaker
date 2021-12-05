88d420c4b8eef175fd793f9d8cbe82d0df02cfc3
Djamel Ramdani <enzonpapa@gmail.com>
2021-12-04 20:29:07 -0600

    api

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+const getNotes = require("./db/db.json");
+app.get("/api/notes", (req, res) => {
+  res.json(getNotes.slice(1));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
